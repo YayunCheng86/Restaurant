903d2c3acac41fff148cbe895ea98f0a3ee8a0de
Laura <lolacheng86@gmail.com>
2020-03-03 06:07:27 +0800

    Add confirm pop-up box when delete

---
+++ main.js
@@ -0,0 +1,20 @@
+function deleteConfirm () {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((isDelete) => {
+        if (isDelete) {
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+        }
+        return false;
+    })
+}
