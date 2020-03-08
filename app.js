67045cec43b069640b9bef8969bc35525cb1b802
Laura <lolacheng86@gmail.com>
2020-03-08 09:12:20 +0800

    Install express-session

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // 連線資料庫
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -18,18 +25,17 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-
+// 註冊session
+app.use(session({
+    secret: 'QOO is the best drink ever',
+    resave: false,
+    saveUninitialized: true
+}))
 
 // routes
 app.use('/', require('./routes/home'))
 app.use('/restaurants', require('./routes/restaurant'))
 app.use('/users', require('./routes/user'))
-
 
 app.listen(port, () => {
     console.log(`Express is listening on localhost:${port}/restaurants`)
